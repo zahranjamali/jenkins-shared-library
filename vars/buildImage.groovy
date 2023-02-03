0623903c6cf3261a87846ec98d91fb0102e462b5
zahran <zahranjamali@gmail.com>
2023-02-03 17:10:54 +0530

    zahran changes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+def call () {
+    echo "building the application"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh 'docker build -t zahranjamali/my-repo:jma-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push zahranjamali/my-repo:jma-1.0'
     }
 }
