b7c99d6b60eecd64a873f01c9909b17653a650e2
zahran <zahranjamali@gmail.com>
2023-02-03 20:12:56 +0530

    docker.grrovy

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+package com.example
+class Docker implements Serializable  {
+    def script
+    Docker(script){
+        this.script = script
+    }
+    def buildDockerImage (String imageName) {
+        script.echo "building the docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            script.sh "docker push $script.imageName"
+        }
+    }
+}
